a36972c34795be8c547619ad4cc23569c348b9d5
yakkshit <63894727+saiyakkshit@users.noreply.github.com>
2022-07-03 13:11:27 +0530

    Create Remove first n characters from a string.py

---
+++ basic_programs/Remove first n characters from a string.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    l = input("Enter a string")
+    n = list(l)
+    j = int(input("enter number of characer to be removed "))
+    while j < len(n):
+        print(n[j])
+        j += 1
