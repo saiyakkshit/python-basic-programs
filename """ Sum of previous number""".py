68c32a3a48ac193f76118e8d2ffa5276a91f417e
yakkshit <63894727+saiyakkshit@users.noreply.github.com>
2022-07-03 12:34:11 +0530

    Add files via upload

---
+++ """ Sum of previous number""".py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    numer = int(input("Enter a number to be printed"))
+    n = []
+    k = 0
+    for i in range(numer):
+        n.append(i)
+        k = n[i-1]
+        j = k + n[i]
+        print(" current number is ", n[i],
+              " previous number is ", k, " sum is ", j)
